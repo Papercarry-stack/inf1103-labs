931c152b04f8cdc54d114ede55e08e9736c0d824
Jia Hua <2605754@sit.singaporetech.edu.sg>
2026-09-04 14:45:21 +0800

    first update

--- hello.py
+++ hello.py
@@ -1,5 +1,13 @@
 print ("================================")
-
 print ("Hello, World!")
 print ("My first post!")
 print ("================================")
+
+
+username = "Jia Hua"
+Bio = "I love dogs."
+followers = 10
+
+print ("username: ", username)
+print ("bio: ", Bio)
+print ("followers: ", followers)
