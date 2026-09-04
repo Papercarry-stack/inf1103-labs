7339597cfef5bf19e2c640bcb568a656c91be9c6
Jia Hua <2605754@sit.singaporetech.edu.sg>
2026-09-04 14:49:38 +0800

    updated follower count

--- hello.py
+++ hello.py
@@ -11,3 +11,12 @@ followers = 10
 print ("username: ", username)
 print ("bio: ", Bio)
 print ("followers: ", followers)
+
+followers += 50
+print ("Day 1: ", followers)
+
+followers += 30
+print ("Day 2: ", followers)
+
+followers -= 10
+print ("Day 3: ", followers)
